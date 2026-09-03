94aceeaef28d51f6b4fe4c323e7d6ab182b3bfbb
robbieflynn <mrflynnib@gmail.com>
2026-09-03 17:36:27 +0400

    Refresh cached question bank embeds (#15)
    * Add temporary question whiteboards
    
    * Refresh cached question bank embeds
    
    ---------
    
    Co-authored-by: Robert Flynn <rob@Roberts-MacBook-Air-2.local>

--- src/components/igcse-question-bank-embed.tsx
+++ src/components/igcse-question-bank-embed.tsx
@@ -77,7 +77,7 @@ export function IgcseQuestionBankEmbed() {
       loading="eager"
       ref={frameRef}
       scrolling="no"
-      src="/question-bank/igcse-bank.html?embedded=1"
+      src="/question-bank/igcse-bank.html?embedded=1&whiteboard=1"
       style={{ height: `${frameHeight}px` }}
       title="Edexcel IGCSE Mathematics question bank"
     />
